d287a55edda8f52236c953e07fc12c235762f436
joshgirard21 <joshgirard21@gmail.com>
2019-04-11 14:31:24 -0400

    building conditions added

--- game.js
+++ game.js
@@ -11,16 +11,20 @@ var config = {
     update: update
   }
 };
-
 var solar = new Phaser.Game(config);
+
 var StartButton;
 var Stage;
 var FlatMirror;
+var Sun;
 var x;
 var y;
 var locx = [];
 var locy = [];
 var mirrornum = -1;
+var canplace = true;
+var sunx = 800;
+var suny = 0;
 function preload() {
     this.load.path = 'assets/';
     this.load.image('StartButton','StartButton.png');
@@ -48,7 +52,7 @@ function create(){
     frameRate: 4,
     repeat: -1
   });
-  this.add.sprite(650,150,'sun').setScale(4).play('sun');
+  Sun = this.add.sprite(650,150,'sun').setScale(4).play('sun');
   StartButton.on('pointerover', function(){
     StartButton.setTexture('StartButtonPressed');
   });
@@ -59,11 +63,11 @@ function create(){
     part = 1;
   });
   this.input.on('pointerdown',function(cursor){
-    if (part == 2){
+    if (part == 2 && canplace){
       locx.push(cursor.x);
       locy.push(cursor.y);
       mirrornum = mirrornum + 1;
-      this.add.sprite(locx[mirrornum],locy[mirrornum],'FlatMirror').setRotation((Math.PI)/2+Math.atan((locy[mirrornum]-300)/(locx[mirrornum]-400)));
+      this.add.sprite(locx[mirrornum],locy[mirrornum],'FlatMirror').setRotation(3*(Math.PI)/2+Math.atan((locy[mirrornum]-suny)/(locx[mirrornum]-sunx)));
     }
   },this);
 }
@@ -71,13 +75,25 @@ function update(){
   x = this.input.x;
   y = this.input.y;
   if (part == 1){
+    help = this.add.text(100,550,'').setFontSize(45).setColor('black').setFontFamily('Superscript');
     StartButton.destroy();
-    Stage.setVisible(true).setInteractive();
+    Stage.setVisible(true);
     FlatMirror.setVisible(true);
+    Sun.setPosition(sunx,suny);
     part = 2;
   }
   if  (part == 2){
+    help.text = 'Good Spot to Build';
     FlatMirror.setPosition(x,y);
+    FlatMirror.setTintFill(0x008000);
+    canplace = true;
+    for (var i = 0; i<= locx.length; i++){
+      if (Math.abs(locx[i]-x)<30 && Math.abs(locy[i]-y)<30){
+        FlatMirror.setTintFill(0xFF0000);
+        canplace = false;
+        help.text = 'Too Close!'
+      }
+    }
   }
 
 }
