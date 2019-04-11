5a11f483862572a63dcad3afda33330fe3e03b66
joshgirard21 <joshgirard21@gmail.com>
2019-04-10 21:57:29 -0400

    stage

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
-color = '66, 100, 200';
+//alert("Welcome!");
+var go = 0;
+var color = '66, 100, 200';
 var config = {
   type:Phaser.AUTO,
   width:800,
@@ -12,12 +14,15 @@ var config = {
 };
 
 var solar = new Phaser.Game(config);
-
+var StartButton
+var Stage
+var FlatMirror
 function preload() {
-
     this.load.path = 'assets/';
     this.load.image('StartButton','StartButton.png');
     this.load.image('StartButtonPressed','StartButtonPressed.png');
+    this.load.image('Stage','Stage.png');
+    this.load.image('FlatMirror','FlatMirror.png');
     this.load.image('Stage','Stage.png');
     this.load.image('FlatMirror','FlatMirror.png');
 
@@ -27,22 +32,29 @@ function preload() {
 
 function create(){
 
-  var StartButton = this.add.sprite(400,300,'StartButton').setScale(1.5).setInteractive();
+  StartButton = this.add.sprite(400,300,'StartButton').setScale(1.5).setInteractive();
 
+  StartButton.inputEnabled = true;
   StartButton.on('pointerover', function(){
     StartButton.setTexture('StartButtonPressed');
   });
   StartButton.on('pointerout', function(){
     StartButton.setTexture('StartButton');
   });
-  this.input.on('pointermove', function (cursor){
-    x = cursor.x;
-    y = cursor.y;
-    Xtext.text = 'X coord is:'+x;
-    Ytext.text = 'Y coord is:'+y;
-  }, this);
+  StartButton.on('pointerdown',function(){
+    go = 1;
+  });
+
 }
+function update(){
+  x = this.input.mousePointer.x;
+  y = this.input.mousePointer.y;
+  Xtext.text = 'X coord is:'+x;
+  Ytext.text = 'Y coord is:'+y;
+  if (go == 1){
+    StartButton.destroy();
+    Stage = this.add.sprite(400,300,'Stage');
+    FlatMirror = this.add.sprite(x,y,'FlatMirror');
 
-function update(delta) {
-
+  }
 }
