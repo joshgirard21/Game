f0226460e421a0ab7ba1b88869fd35a700169741
joshgirard21 <joshgirard21@gmail.com>
2019-04-12 17:41:20 -0400

    added rotation fix and fonts

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-var part = 0;
-var color = '66, 100, 200';
+var part = 0; //the section of the game which it is in
+var color = '66, 100, 200'; //bg color
 var config = {
   type:Phaser.AUTO,
   width:800,
@@ -13,23 +13,24 @@ var config = {
 };
 var solar = new Phaser.Game(config);
 
-var StartButton;
-var Stage;
-var FlatMirror;
-var Sun;
-var GoButton;
-var Panels = [];
-var x;
-var y;
-var locx = [];
-var locy = [];
-var mirrornum = -1;
-var canplace = true;
-var sunx = 800;
-var suny = 0;
-var panelcount = -1;
+var StartButton; //white start button
+var Stage; //cactus bg
+var FlatMirror; //mirror sprite
+var Sun; //sun animation
+var GoButton; //red go button
+var Panels = [0]; //list of all the panel objects
+var x; //cursor x
+var y; //cursor y
+var locx = []; //list of panel x locations
+var locy = []; //list of panel y locations
+var mirrornum = -1; //number of mirrors placed
+var canplace = true; // true means mirrors can be placed
+var sunx = 800; // sun start x
+var suny = 0; // sun start y
+var onGoButton = false;
+var sunset = -256;
 function preload() {
-    this.load.path = 'assets/';
+    this.load.path = 'assets/'; // this.load goes right to /assets
     this.load.image('StartButton','StartButton.png');
     this.load.image('StartButtonPressed','StartButtonPressed.png');
     this.load.image('Stage','Stage.png');
@@ -43,9 +44,9 @@ function preload() {
 }
 
 function create(){
-
   StartButton = this.add.sprite(400,300,'StartButton').setScale(1.5).setInteractive();
   Stage = this.add.sprite(400,300,'Stage').setVisible(false);
+  moremirrors = this.add.text(125,300,'').setFontSize(45).setColor('black').setFontFamily('Superscript');
   FlatMirror = this.add.sprite(400,300,'FlatMirror').setVisible(false);
   GoButton = this.add.sprite(700,500,'GoButton').setScale(2).setInteractive().setVisible(false);
   this.anims.create({
@@ -68,22 +69,32 @@ function create(){
     part = 1;
   });
   this.input.on('pointerdown',function(cursor){
-    if (part == 2 && canplace){
+    if (part == 2 && canplace && !onGoButton){
       locx.push(cursor.x);
       locy.push(cursor.y);
       mirrornum = mirrornum + 1;
-      panelcount = panelcount +1;
-      Panels[panelcount] = this.add.sprite(locx[mirrornum],locy[mirrornum],'FlatMirror').setRotation(3*(Math.PI)/2+Math.atan((locy[mirrornum]-suny)/(locx[mirrornum]-sunx)));
+      Panels[mirrornum] = this.add.sprite(locx[mirrornum],locy[mirrornum],'FlatMirror').setRotation(3*(Math.PI)/2+Math.atan((locy[mirrornum]-suny)/(locx[mirrornum]-sunx)));
     }
   },this);
   GoButton.on('pointerover', function(){
     GoButton.setTint(0xA9A9A9);
+    onGoButton = true;
   });
   GoButton.on('pointerout', function(){
+    if (part == 2){
     GoButton.clearTint();
+    onGoButton = false;
+    moremirrors.text = '';
+  }
   });
   GoButton.on('pointerdown', function(){
+    if (Panels[0] == 0){
+      moremirrors.text = 'Please Place a Mirror!';
+    }else{
     part = 3;
+    canplace = false;
+    GoButton.setTint(0xA9A9A9);
+    }
   });
 }
 function update(){
@@ -112,14 +123,29 @@ function update(){
     }
   }
   if (part == 3){
+    if (sunx < 100){
+      sunset--;
+      suny++;
+      Stage.tint = sunset;
+    }
+    if (suny > 250){
+      part = 4;
+    }
     FlatMirror.setVisible(false);
     help.destroy();
     sunx--;
-    Sun.setPosition(sunx,suny)
     for (var i = 0; i< Panels.length; i++){
-      thispanel = Panels[i];
-      thispanel.setRotation(3*(Math.PI)/2+Math.atan((locy[i]-suny)/(locx[i]-sunx)));
+      var thispanel = Panels[i];
+      var angleRad = 3*(Math.PI)/2+Math.atan((locy[i]-suny)/(locx[i]-sunx));
+      if (locx[i] >= sunx){
+        thispanel.setRotation(angleRad+Math.PI);
+      }else{
+        thispanel.setRotation(angleRad);
+    }
+    Sun.setPosition(sunx,suny);
     }
   }
-
+  if (part == 4){
+    GoButton.destroy();
+  }
 }
