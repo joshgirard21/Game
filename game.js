1a45b61e9413650d506923d49b62bb64f4d2b3d0
joshgirard21 <joshgirard21@gmail.com>
2019-04-15 00:27:22 -0400

    added tower, new favicon, added sun tracking and bug fix

--- game.js
+++ game.js
@@ -18,17 +18,21 @@ var Stage; //cactus bg
 var FlatMirror; //mirror sprite
 var Sun; //sun animation
 var GoButton; //red go button
+var Tower;
 var Panels = [0]; //list of all the panel objects
+var Line = [];
 var x; //cursor x
 var y; //cursor y
 var locx = []; //list of panel x locations
 var locy = []; //list of panel y locations
+var towerx = 0;
+var towery;
 var mirrornum = -1; //number of mirrors placed
 var canplace = true; // true means mirrors can be placed
 var sunx = 800; // sun start x
 var suny = 0; // sun start y
 var onGoButton = false;
-var sunset = -256;
+var sunset = 1;
 function preload() {
     this.load.path = 'assets/'; // this.load goes right to /assets
     this.load.image('StartButton','StartButton.png');
@@ -39,16 +43,21 @@ function preload() {
     this.load.image('FlatMirror','FlatMirror.png');
     this.load.image('Sun1','Sun1.png');
     this.load.image('Sun2','Sun2.png');
-    this.load.image('GoButton','GoButton.png')
+    this.load.image('GoButton','GoButton.png');
+    this.load.image('Tower','CollectorTower.png');
 
 }
 
 function create(){
   StartButton = this.add.sprite(400,300,'StartButton').setScale(1.5).setInteractive();
   Stage = this.add.sprite(400,300,'Stage').setVisible(false);
-  moremirrors = this.add.text(125,300,'').setFontSize(45).setColor('black').setFontFamily('Superscript');
+  moremirrors = this.add.text(100,300,'').setFontSize(45).setColor('black').setFontFamily('Superscript');
   FlatMirror = this.add.sprite(400,300,'FlatMirror').setVisible(false);
   GoButton = this.add.sprite(700,500,'GoButton').setScale(2).setInteractive().setVisible(false);
+  Tower = this.add.sprite(0,0,'Tower').setVisible(false);
+  for (var i = 0; i <12; i++){
+    Line[i] = this.add.graphics();
+  }
   this.anims.create({
     key: 'sun',
     frames: [
@@ -69,27 +78,37 @@ function create(){
     part = 1;
   });
   this.input.on('pointerdown',function(cursor){
-    if (part == 2 && canplace && !onGoButton){
+    if (part == 2 && canplace && !onGoButton && Panels.length < 12){
       locx.push(cursor.x);
       locy.push(cursor.y);
       mirrornum = mirrornum + 1;
       Panels[mirrornum] = this.add.sprite(locx[mirrornum],locy[mirrornum],'FlatMirror').setRotation(3*(Math.PI)/2+Math.atan((locy[mirrornum]-suny)/(locx[mirrornum]-sunx)));
+      if (mirrornum == 11){
+        part = 2.5;
+      }
+    }
+    if (part == 2.75 && canplace){
+      this.add.sprite(cursor.x,cursor.y,'Tower');
+      towerx = cursor.x;
+      towery = cursor.y;
+      part = 2.8;
     }
   },this);
   GoButton.on('pointerover', function(){
     GoButton.setTint(0xA9A9A9);
     onGoButton = true;
+    canplace = false;
   });
   GoButton.on('pointerout', function(){
-    if (part == 2){
+    if (part == 2 || part == 2.75){
     GoButton.clearTint();
     onGoButton = false;
     moremirrors.text = '';
   }
   });
   GoButton.on('pointerdown', function(){
-    if (Panels[0] == 0){
-      moremirrors.text = 'Please Place a Mirror!';
+    if (Panels[0] == 0 || towerx == 0){
+      moremirrors.text = 'Place Mirrors & Collector!';
     }else{
     part = 3;
     canplace = false;
@@ -118,18 +137,48 @@ function update(){
       if (Math.abs(locx[i]-x)<30 && Math.abs(locy[i]-y)<30){
         FlatMirror.setTintFill(0xFF0000);
         canplace = false;
-        help.text = 'Too Close!'
+        help.text = 'Too Close!';
       }
     }
   }
+  if (part == 2.5){
+    Tower.setVisible(true);
+    FlatMirror.setVisible(false);
+    part = 2.75;
+  }
+  if (part == 2.75){
+    Tower.setPosition(x,y);
+    help.text = 'Good Spot to Build';
+    Tower.setTintFill(0x008000);
+    canplace = true;
+    for (var i = 0; i<= locx.length; i++){
+      if (Math.abs(locx[i]-x)<30 && Math.abs(locy[i]-y)<30){
+        Tower.setTintFill(0xFF0000);
+        canplace = false;
+        help.text = 'Too Close!';
+      }
+      if(onGoButton){
+        canplace = false;
+        Tower.setTintFill(0xFF0000);
+      }
+    }
+  }
+  if (part == 2.8){
+    canplace = false;
+    Tower.setVisible(false);
+  }
   if (part == 3){
+    for (var i = 0; i <12; i++){
+    thisline = Line[i];
+    thisline.clear();
+    }
     if (sunx < 100){
-      sunset--;
+      sunset = sunset - .0014;
       suny++;
-      Stage.tint = sunset;
+      Stage.alpha = sunset;
     }
     if (suny > 250){
-      part = 4;
+      part = 5;
     }
     FlatMirror.setVisible(false);
     help.destroy();
@@ -143,9 +192,17 @@ function update(){
         thispanel.setRotation(angleRad);
     }
     Sun.setPosition(sunx,suny);
+    thisline = Line[i];
+    thisline.lineStyle(10,0xffff00);
+    thisline.beginPath(); thisline.moveTo(sunx,suny); thisline.lineTo(locx[i],locy[i]); thisline.closePath(); thisline.strokePath();
     }
   }
-  if (part == 4){
+  if (part == 5){
     GoButton.destroy();
+    for (var i = 0; i <12; i++){
+    thisline = Line[i];
+    thisline.clear();
+    }
+    part = 6;
   }
 }
